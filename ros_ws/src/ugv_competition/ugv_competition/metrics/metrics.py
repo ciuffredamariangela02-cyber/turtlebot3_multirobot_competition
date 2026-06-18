4dc5a398e4c8038591e9f35443a0c921e9840107
alessandrabonavita <104979463+alessandrabonavita@users.noreply.github.com>
2026-06-18 19:13:38 +0200

    Update metrics.py

--- ros_ws/src/ugv_competition/ugv_competition/metrics/metrics.py
+++ ros_ws/src/ugv_competition/ugv_competition/metrics/metrics.py
@@ -1,23 +1,27 @@
-import rclpy
-from rclpy.node import Node
-from geometry_msgs.msg import PoseWithCovarianceStamped, PoseArray, PoseStamped
-from std_msgs.msg import String
-from nav2_msgs.action import NavigateToPose
+# Alternative metric to estimate distance from goal
+
+import math
 from tf_transformations import euler_from_quaternion
 import math
     
-    # Alternative metric to estimate distance from goal
-    def manhattan_distance(self,pose,goal):
+
+# Constants for estimated time calculation
+VX_MAX = 0.22
+WZ_MAX = 0.8 
+
+def manhattan_distance(pose,goal):
         '''Calculate Manhattan distance between a pose and a goal'''
         dx = abs(pose.position.x - goal['x'])
         dy = abs(pose.position.y - goal['y'])
-        return dx+dy
+        
+        return dx + dy
 
-    # Alternative metric to estimate distance from goal
-    def estimated_time_to_goal(self,pose,goal):
+
+
+def estimated_time_to_goal(pose,goal):
         dx = pose.position.x - goal['x']
         dy = pose.position.y - goal['y']
-        dist= math.hypot(dx, dy)
+        dist = math.hypot(dx, dy)
         
         orientation=pose.orientation
         q = [orientation.x, orientation.y, orientation.z, orientation.w]
@@ -30,12 +34,14 @@ import math
         
         time_to_rotate = angle_diff/WZ_MAX
         time_to_translate = dist/VX_MAX
-        return time_to_rotate+time_to_translate #estimated seconds
+        estimated_seconds = time_to_rotate + time_to_translate
+        return estimated_seconds
 
-    # alternative metric for score_goal method
-    def cluster_score(self, goal, radius=2.0):
+# alternative metric for score_goal method
+def cluster_score( goal, all_goal, radius=2.0):
+        """Count how many other goals are within a certain radius of the target goal."""
         count = 0
-        for other in self.goals:
+        for other in all_goal:
             if other == goal:
                 continue
             d = math.hypot(goal['x'] - other['x'], goal['y'] - other['y'])
@@ -43,9 +49,11 @@ import math
                 count += 1
         return count 
 
-    def euclidean_distance(self, pose, goal):
+
+def euclidean_distance( pose, goal):
         """Calculate Euclidean distance between a pose and a goal."""
         dx = pose.position.x - goal['x']
         dy = pose.position.y - goal['y']
         return math.hypot(dx, dy)
     
+
